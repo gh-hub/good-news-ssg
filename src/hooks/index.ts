24c129aa6ca1456620457cee30693c3b914a218b
gil hadad <gilhadad@gilhadad.com>
2024-08-23 17:39:27 +0300

    fix build

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,2 @@
 import useFetch from "./useFetch";
-import useFetchRss from "./rss/useFetchRssFeed";
-import { RSS_TEST_DATA } from "./rss/examples";
-export { useFetch, useFetchRss, RSS_TEST_DATA };
+export { useFetch };
